5b5ba92faa3a1a898774327562fbc67712ae04c7
Katya-oooops <courage.wisdom.force@gmail.com>
2019-10-12 14:48:27 +0300

    fix:second

--- src/index.js
+++ src/index.js
@@ -7,20 +7,21 @@ let secondArr=String(second).split('').reverse();
 for ( var i=0; i<firstArr.length; i++){
     for ( var j=0; j<secondArr.length; j++) {
         let q=firstArr[i]*secondArr[j];
-        stack[i+j] = 'undefined' ? q : stack[i+j]+q;
+      
+        stack[i+j]= stack[i+j]>= 0 ? stack[i+j]+q : q;
     
     }
     }
     for (let n=0 ; n<stack.length ; n++){
       
       let p=Math.floor(stack[n]/10);
-      (stack[n]=stack[n]%10);
-      if (stack[n+1] = 'undefined' && p!=0){
-        stack[n+1] = p;
-      } else if (stack[n+1]!= 'undefined' && p!=0){
-        stack[n+1] += p;
+      stack[n]=stack[n]%10;
+      if (stack[n + 1]){
+        stack[n + 1] += p;
       }
-    
+      else if (p != 0){
+        stack[n + 1] = p;
+      }
     }
     return stack.reverse().join('');
 }
